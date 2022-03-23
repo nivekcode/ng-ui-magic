0a6a5d79e2e943a1b95f3a28739c0901112e64a9
Kiwi <kiwi@kiwis-macbook-pro-2.home>
2022-03-23 09:50:26 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {DialogComponent} from './dialog/dialog.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {path: 'dialog', loadChildren: './dialog/dialog.module#DialogModule'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {GreetingDirective} from './directive/greeting.directive';
-import {ClickDirective} from './directive/click.directive';
-import {TrackDirective} from './directive/track.directive';
-import {TemplateSampleComponent} from './templates/template-sample.component';
-import {CreateTemplateComponent} from './templates/create-template.component';
-import {TemplateWithDataComponent} from './templates/template-with-data.component';
-import {ThreeDirective} from './directive/three.directive';
-import {ThreeDataDirective} from './directive/three-data.directive';
-import {ThreeDynamicDirective} from './directive/three-dynamic.directive';
-import {MyNgIfDirective} from './directive/myNgIf.directive';
-import {ThreedataIndexDirective} from './directive/threedata-index.directive';
-import {MoveElementsModule} from './move-elements/move-elements.module';
-import {GreenDirective} from './directive/green.directive';
-import {BlueOnClickDirective} from './directive/blue-on-hover.directive';
-import {DialogComponent} from './dialog/dialog.component';
-import {ContentProjectionModule} from './content-projection/simple_example/content-projection.module';
-import {ComponentsExampleModule} from './content-projection/components_example/components-example.module';
-import {GoodEvilModule} from './content-projection/good_evil/good-evil.module';
-import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DialogComponent,
-    GreetingDirective,
-    ClickDirective,
-    TrackDirective,
-    TemplateSampleComponent,
-    CreateTemplateComponent,
-    TemplateWithDataComponent,
-    ThreeDirective,
-    ThreeDataDirective,
-    ThreeDynamicDirective,
-    MyNgIfDirective,
-    ThreedataIndexDirective,
-    GreenDirective,
-    BlueOnClickDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MoveElementsModule,
-    ContentProjectionModule,
-    ComponentsExampleModule,
-    GoodEvilModule
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [DialogComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
